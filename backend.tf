fbc79b210db379119555d507e11c792233ff8e1b
jcaparob3 <javier.fernando.caparo.becerra@nttdata.com>
2023-01-07 21:49:57 -0500

    add more servers

--- backend.tf
+++ backend.tf
@@ -2,10 +2,10 @@
 # For more information, see https://www.terraform.io/docs/backends/types/remote.html
 terraform {
   backend "remote" {
-    organization = "{{ORGANIZATION_NAME}}"
+    organization = "example-org-9c3b8f"
 
     workspaces {
-      name = "{{WORKSPACE_NAME}}"
+      name = "getting-started"
     }
   }
 
